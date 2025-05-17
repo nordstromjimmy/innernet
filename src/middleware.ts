b5adfb0f641b608b89e245a312fd40b17f6c6edb
Jimmy <nordstromjimmy@gmail.com>
2025-05-17 13:24:12 +0200

    update

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,7 +8,7 @@ export function middleware(req: NextRequest) {
   const isLandingPage = pathname === "/";
 
   // Optional: Add a launch toggle
-  const isAppLive = process.env.APP_LIVE === "true";
+  const isAppLive = "false";
 
   if (isLandingPage || isAppLive) {
     return NextResponse.next();
